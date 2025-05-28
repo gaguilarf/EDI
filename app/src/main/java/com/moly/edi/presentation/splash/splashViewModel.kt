d9764a106f5f1cf84d19fc23687dddb09bae2592
HeidiStephanyCordovaSilva <88119136+HeidiCordova@users.noreply.github.com>
2025-05-27 18:55:03 -0600

    Update splashViewModel.kt

--- app/src/main/java/com/moly/edi/presentation/splash/splashViewModel.kt
+++ app/src/main/java/com/moly/edi/presentation/splash/splashViewModel.kt
@@ -1,4 +1,25 @@
 package com.moly.edi.presentation.splash
 
-class splashViewModel {
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class SplashViewModel : ViewModel() {
+
+    private val _welcomeText = MutableStateFlow("BIENVENIDO")
+    val welcomeText: StateFlow<String> = _welcomeText
+
+    private val _navigateToLogin = MutableStateFlow(false)
+    val navigateToLogin: StateFlow<Boolean> = _navigateToLogin
+
+    init {
+        viewModelScope.launch {
+            delay(3000)
+            _navigateToLogin.value = true
+        }
+    }
 }
